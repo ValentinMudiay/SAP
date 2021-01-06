a471e53e4f36e7849825ebab83fc91a3fddb24d1
Jon George <23463258+JonGeorge@users.noreply.github.com>
2021-01-06 05:59:57 -0500

    Validate user is authenticated before using search

--- api/authChecker.js
+++ api/authChecker.js
@@ -1,6 +1,5 @@
 module.exports = {
     viaSpotify: (req, res, next) => {
-        console.log("Checking auth");
         return req.session.access_token ? next() : res.redirect("/");
     },
 };
--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 
-const helmet    = require("helmet"),
-      config    = require("./config/app"),
-      isAuth    = require("./api/authChecker"), // To be used for routes that require authentication
-      authApi   = require("./api/spotifyLogin"),
-      rootApi   = require("./api/index"),
-      searchApi = require("./api/spotifySearch"),
-      express   = require("express"),
-      app       = express();
+const helmet     = require("helmet"),
+      config     = require("./config/app"),
+      isLoggedIn = require("./api/authChecker"),
+      authApi    = require("./api/spotifyLogin"),
+      rootApi    = require("./api/index"),
+      searchApi  = require("./api/spotifySearch"),
+      express    = require("express"),
+      app        = express();
 
 app.use(config.session);
 app.use(helmet());
 app.use("/", rootApi);
 app.use("/login", authApi);
-app.use("/search", searchApi);
+app.use("/search", isLoggedIn.viaSpotify, searchApi);
 
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
