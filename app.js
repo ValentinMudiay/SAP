ce8e1ce8426960c428ecc07eb500ed09c6c3a1eb
Jon George <23463258+JonGeorge@users.noreply.github.com>
2021-01-06 21:16:43 -0500

    Update 'under construction' page and set up email capture

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const rootApi    = require("./api/index"),
       isLoggedIn = require("./api/authChecker"),
       authApi    = require("./api/spotifyLogin"),
       searchApi  = require("./api/spotifySearch"),
+      emailApi   = require("./api/emailCapture"),
       underConstruction = require("./api/underConstruction");
 
 // App Config
 const config     = require("./config/app");
       
 // NPM Packages
-const path       = require("path"),
+const bodyParser = require("body-parser"),
+      path       = require("path"),
       helmet     = require("helmet"),
       express    = require("express"),
       app        = express();
@@ -19,10 +21,12 @@ const path       = require("path"),
 app.use(config.session);
 app.use(helmet());
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", underConstruction, rootApi);
 app.use("/login", underConstruction, authApi);
 app.use("/search", underConstruction, isLoggedIn.viaSpotify, searchApi);
+app.use("/notify-launch", emailApi);
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
 app.listen(config.port, () => console.log(`Listening on port ${config.port}`));
