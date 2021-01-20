fa7f6dcb8e00cf1732293df814b1de79df62a2b5
Jon George <23463258+JonGeorge@users.noreply.github.com>
2021-01-20 12:59:29 -0500

    Add UI for testing type ahead search and saving playlist

--- public/typeahead.js
+++ public/typeahead.js
@@ -1,5 +1,6 @@
 const searchInput = document.getElementById("search");
 
+
 /**
  * Executes AJAX api call to fetch search results.
  * 
@@ -23,11 +24,72 @@ searchInput.addEventListener("keyup", (event) => {
     if(searchStrNoWhiteSpace.length >= minimumCharsForTypeahead) {
         search(input)
         .then(response => {
-            console.log(response); // TODO: Handle repsonse with search results
+            // console.log(response); // TODO: Handle repsonse with search results
+            const playlists = response.playlists.items;
+            displayPlaylistResults(playlists);
+        })
+        .catch(error => {
+            console.error(error);
         });
     }
 });
 
+function displayPlaylistResults(playlists) {
+    const ol = document.createElement("ol");
+
+    for(let i = 0; i < playlists.length; i++) {
+        const addBtn = getAddBtn(playlists[i]);
+
+         const li = document.createElement("li");
+         li.appendChild(addBtn);
+         li.append(playlists[i].name);
+         ol.appendChild(li);
+    }
+
+    const body = document.getElementById("body");
+
+    const newResultsDiv = document.createElement("div");
+    newResultsDiv.setAttribute("id", "result");
+    newResultsDiv.appendChild(ol); // add list to new results div
+
+    const currentResultsDiv  = document.getElementById("result");
+    body.replaceChild(newResultsDiv, currentResultsDiv); // replace results div
+}
+
+function getAddBtn(playlist) {
+    const addBtn = document.createElement("button");
+    addBtn.innerText = " + ";
+
+    const playlistId = playlist.id;
+    
+    addBtn.addEventListener("click", event => {
+        addPlaylist(playlistId)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    });
+    return addBtn;
+}
+
+function addPlaylist(playlistId) {
+    const data = {
+        playlistId: playlistId
+    };
+
+    return fetch("/save", {
+       "method": "POST",
+       "body": JSON.stringify(data),
+       "headers": {
+           "Content-Type": "application/json",
+       }
+   })
+    .then(res => res.json())
+    .then(data => data)
+    .catch(err => console.error(err));
+}
 
 /* Returned playlist
     {
