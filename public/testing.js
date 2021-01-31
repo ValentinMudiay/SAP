cee060ef35e1c6d1f129c69e022afd37aa10342a
Jon George <23463258+JonGeorge@users.noreply.github.com>
2021-01-31 07:50:36 -0500

    Update 'a' elements to buttons

--- public/testing.js
+++ public/testing.js
@@ -1,39 +1,4 @@
 function addEventListeners() {
-
-    const userBtn = document.getElementById("getId");
-    userBtn.addEventListener("click", function(event) {
-        console.log("userBtn clicked");
-
-        // fetch("/save")
-        // .then(response => response.text())
-        // .then(data => console.log(data))
-        // .catch(err => console.error(err));
-
-    });
-
-    const createPlaylistBtn = document.getElementById("createEmptyPlaylist");
-    createPlaylistBtn.addEventListener("click", function(event) {
-        console.log("createPlaylistBtn clicked");
-
-        const data = { tracksUrl: "" };
-
-        const dataStr = jsonToQueryStr(data);
-
-        const options = {
-            "method": "POST",
-            "body": dataStr,
-            "headers": {
-                "Content-Type": "application/x-www-form-urlencoded",
-            }
-        };
-
-        fetch("/save", options)
-        .then(response => response.text())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-
-    });
-
     function poptastic(url) {
         var newWindow = window.open(url, 'Spotify Login', 'toolbar=no, menubar=no, status=no, directories=no, height=750, width=600');
         if (window.focus) {
