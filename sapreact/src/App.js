780e4ab1deb0996e3bd641855cffdb716e860b2f
ValentinMudiay <vmudia1@students.towson.edu>
2021-03-31 00:23:03 -0400

    Added green playlist add icon

--- sapreact/src/App.js
+++ sapreact/src/App.js
@@ -4,25 +4,29 @@ function App() {
   return (
     <div>
       <header>
-        <img className="playlist-icon" src="./playlist-add.svg" alt="Add a playlist icon"/>
+        <img className="playlist-icon" src="./playlist-add-green.svg" alt="Add a playlist icon"/>
         <span>SAVE A PLAYLIST</span>
       </header>
-      <div className="content">
-        <img className="panda" src="./panda.gif" alt="Waving panda"/>
-        <h1>COMING SOON</h1>
-        <p>We'll be launching soon, subscribe to be notified when we do.</p>
-        <form action="/notify-launch" method="post">
-          <div className="input">
-            <label for="email">Email</label>
-            <input type="email" name="email" placeholder="Email Address" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" autofocus required/>
-          </div>
-          <div className="notify-btn">
-            <input type="submit" value="Notify Me"/>
-          </div>
-        </form>
-        
+      <main>
+        <div className="content">
+          <img className="panda" src="./panda.gif" alt="Waving panda"/>
+          <h1>COMING SOON</h1>
+          <p>We'll be launching soon, subscribe to be notified when we do.</p>
+          <form action="/notify-launch" method="post">
+            <div className="input">
+              <label for="email">Email</label>
+              <input type="email" name="email" placeholder="Email Address" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" autofocus required/>
+            </div>
+            <div className="notify-btn">
+              <input type="submit" value="Notify Me"/>
+            </div>
+          </form>
+        </div>
+      </main>
+      <footer>
+        Hi
+      </footer>
 
-      </div>
     </div>
   );
 }
